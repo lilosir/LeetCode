ac58526c2a964c1d157d28090502758352f84dfb
lilosir <sryoliver@gmail.com>
2020-03-02 11:21:55 -0500

    add problem  1021

--- problem1021/problem1021.go
+++ problem1021/problem1021.go
@@ -1,44 +1,44 @@
 package problem1021
 
+import "strings"
+
+// func removeOuterParentheses(S string) string {
+// 	result := ""
+// 	open := 0
+
+// 	for i := 0; i < len(S); i++ {
+// 		if S[i] == '(' {
+// 			open++
+// 			if open != 1 {
+// 				result += "("
+// 			}
+// 		} else {
+// 			open--
+// 			if open != 0 {
+// 				result += ")"
+// 			}
+// 		}
+// 	}
+// 	return result
+// }
+
+// user strings Builder will use less memory!!
 func removeOuterParentheses(S string) string {
-	result := ""
+	var result strings.Builder
 	open := 0
 
 	for i := 0; i < len(S); i++ {
 		if S[i] == '(' {
 			open++
 			if open != 1 {
-				result += "("
+				result.WriteRune('(')
 			}
 		} else {
 			open--
 			if open != 0 {
-				result += ")"
+				result.WriteRune(')')
 			}
 		}
 	}
-	return result
+	return result.String()
 }
-
-// user strings Builder will use less memory!!
-// func removeOuterParentheses(S string) string {
-//     var level = 0
-// 	var result strings.Builder
-//     result.Grow(len(S))
-
-// 	for i := 0; i < len(S); i++ {
-// 		var c = S[i]
-// 		if c == '(' {
-// 			level++
-// 			if level > 1 {
-// 				result.WriteByte(c)
-// 			}
-// 		} else {
-// 			level--
-// 			if level > 0 {
-// 				result.WriteByte(c)
-// 			}
-// 		}
-// 	}
-// 	return result.String()
-// }
